0173f50c798581a820e64005671a07ab75c8cdc8
Tyler D. Franklin <irishlep84@gmail.com>
2017-12-21 15:48:28 -0600

    Assignment 3 complete

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -2,6 +2,11 @@ require 'sqlite3'
 
 module Selection
     def find(*ids)
+        ids.each do |id|
+            unless id.is_a? Numeric && id > 0
+                raise ArgumentError.new('ID must be a number and greater than 0')
+            end
+        end
 
         if ids.length == 1
             find_one(ids.first)
@@ -34,6 +39,10 @@ module Selection
     end
 
     def take(num=1)
+        unless num.is_a? Numeric && num > 0
+            raise ArgumentError.new('Must be a number and greater than 0')
+        end
+
         if num > 1
             rows = connection.execute <<-SQL
                 SELECT #{columns.join ","} FROM #{table}
@@ -81,6 +90,45 @@ module Selection
         SQL
 
         rows_to_array(rows)
+    end
+
+    def some(offset, limit)
+        rows = connection.execute <<-SQL
+            SELECT #{columns.join ","} FROM #{table}
+            ORDER BY id ASC LIMIT #{limit}, #{offset};
+        SQL
+
+        rows_to_array(rows)
+    end
+
+    def method_missing(m, *args, &block)
+        first_part = m[/\A(find_by_)/]
+        second_part = m[/find_by_(\w+)/,1]
+        if first_part = "find_by_"
+            find_by(second_part, args)
+        else
+            return "No #{m} method found"
+        end
+    end
+
+    def find_each(options = {}, &block)
+        if options.empty?
+            records = all
+        else
+            start = options.values.first
+            size = options.values.last
+            records = some(start, size)
+        end
+        records.each do |record|
+            yield(record)
+        end
+    end
+
+    def find_in_batches(options = {}, &block)
+        start = options.values.first
+        size = options.values.last
+        records = some(start, size)
+        yield(records, start)
     end
 
     private

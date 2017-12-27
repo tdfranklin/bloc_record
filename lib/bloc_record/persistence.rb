3e8adf1b55095ca4d198996ab5d15f145d2f557a
Tyler D. Franklin <irishlep84@gmail.com>
2017-12-27 17:06:34 -0600

    ckp5 work complete

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -26,9 +26,21 @@ module Persistence
         SQL
 
         true
+
+        def update_attribute(attribute, value)
+            self.class.update(self.id, { attribute => value })
+        end
+
+        def update_attributes(updates)
+            self.class.update(self.id, updates)
+        end        
     end
 
     module ClassMethods
+        def update_all(updates)
+            update(nil, updates)
+        end
+
         def create(attrs)
             attrs = BlocRecord::Utility.convert_keys(attrs)
             attrs.delete "id"
@@ -42,6 +54,27 @@ module Persistence
             data = Hash[attributes.zip attrs.values]
             data["id"] = connection.execute("SELECT last_insert_rowid();")[0][0]
             new(data)
+        end
+
+        def update(ids, updates)
+            updates = BlocRecord::Utility.convert_keys(updates)
+            updates.delete "id"
+            updates_array = updates.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }
+
+            if ids.class == Fixnum
+                where_clause = "WHERE id = #{ids};"
+            elsif ids.class == Array
+                where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
+            else
+                where_clause = ";"
+            end
+
+            connection.execute <<-SQL
+                UPDATE #{table}
+                SET #{updates_array * ","} #{where_clause}
+            SQL
+
+            true
         end
     end
 end
